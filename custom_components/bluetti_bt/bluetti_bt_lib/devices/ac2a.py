dfa7ad00977299d2d4802ad0d5229dc8b110d2f7
Marco Brambilla <44668241+mbrambilla72@users.noreply.github.com>
2025-09-16 20:17:44 +0200

    .
    BleakClient.connect() called without bleak-retry-connector. For reliable connection establishment,
    use bleak_retry_connector.establish_connection()
    
    revert
    
    HaBleakClientWrapper
    
    revert

--- custom_components/bluetti_bt/bluetti_bt_lib/devices/ac2a.py
+++ custom_components/bluetti_bt/bluetti_bt_lib/devices/ac2a.py
@@ -8,6 +8,7 @@ class AC2A(ProtocolV2Device):
         super().__init__(address, "AC2A", sn)
 
         # Status flags
-        self.struct.add_bool_field('ac_output_on', 1509)
+        self.struct.add_bool_field('ac_output_on', 2011)
         self.struct.add_bool_field('dc_output_on', 2012)
         self.struct.add_bool_field('power_lifting_on', 2021)
+        
